e6e95312f439adea82cf725a1e491d83626e9cc5
VoidNull <nullvoid0div0@protonmail.com>
2024-10-23 10:16:14 -0600

    Hope level 2

---
+++ cmake-build-debug/CMakeFiles/ShutTheBox.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ShutTheBox.dir/main.cpp.obj"
+  "CMakeFiles/ShutTheBox.dir/main.cpp.obj.d"
+  "ShutTheBox.exe"
+  "ShutTheBox.exe.manifest"
+  "ShutTheBox.pdb"
+  "libShutTheBox.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ShutTheBox.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ShutTheBox.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ShutTheBox.
+# This may be replaced when dependencies are built.
